58fc4d76706a43338371763d2850b31bca2b31cf
unknown <group06vp1@yandex.ru>
2020-05-30 17:44:29 +0300

    Base and UserService refactoring

--- services/base_service.py
+++ services/base_service.py
@@ -4,7 +4,6 @@ Date: 2020.05.30
 """
 
 from children import db
-from werkzeug.security import generate_password_hash
 
 
 class BaseService(object):
@@ -15,20 +14,38 @@ class BaseService(object):
     def __init__(self, app):
         self.app = app
 
-    def automatic_id_pre_check(self, entity):
-        # Check that entity is provided
-        if entity is None:
-            raise ValueError('None object was provided to create new entity: {0}'.format(self.__class__.__name__))
+    @classmethod
+    def pre_check_automatic_id(cls, target_function):
+        """Performs a pre-checks for an entity that has automatically generating ID,
+        for example in case if ID is auto-incrementing. Currently the following checks
+        are performing:
+        1. checks whether the entity is None
+        2. checks that ID is not set (it means the entity is already exists)
+        """
+        def wrapper(obj, entity):
+            # Check that entity is provided
+            if entity is None:
+                raise ValueError('None object was provided to create new entity: {0}'.format(obj.__class__.__name__))
 
-        # Check that this entity has no ID
-        if entity.id is not None:
-            raise ValueError('ID presented on creation of new entity: {0}'.format(self.__class__.__name__))
+            # Check that this entity has no ID
+            if entity.id is not None:
+                raise ValueError('ID presented on creation of new entity: {0}'.format(obj.__class__.__name__))
 
-        return True
+            return target_function(obj, entity)
+        return wrapper
 
     def create(self, entity):
+        """Basic creation method, it does nothing special except adding the entity to
+        session, committing it, refreshing the session ensuring the object is up to date
+        and returning it back to caller.
+        """
         db.session.add(entity)
         db.session.commit()
         db.session.refresh(entity)
         return entity
+
+    def get_by_id(self, id):
+        """TODO: implement it, for Lena"""
+        pass
+
 
--- services/user_service.py
+++ services/user_service.py
@@ -15,17 +15,23 @@ class UserService(BaseService):
     def __init__(self, app):
         super().__init__(app)
 
+    @BaseService.pre_check_automatic_id
     def create(self, entity: User):
-        # Perform pre-checks for automatically generated ID
-        super().automatic_id_pre_check(entity)
-
+        """Creates new user calculating a hash of his password. Password is mandatory.
+        The function assumes the password given here in a form of plain string.
+        """
         # Generate password hash on provided password
         if (entity.password is None) or \
            (entity.password == ''):
             raise ValueError('No password was provided on creation of new user')
         entity.password = self.produce_password_hash(entity.password)
 
+        # Call parent's (BaseService) create method
         return super().create(entity)
 
-    def produce_password_hash(self, password):
+    @classmethod
+    def produce_password_hash(cls, password):
+        """Calculates the hash of a given password using standard werkzeug.security
+        function generate_password_hash. No special algorithm taken now, all values are default.
+        """
         return generate_password_hash(password)
