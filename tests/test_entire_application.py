2800c917ddc24278cf58c33e0f42a51567b97c93
unknown <group06vp1@yandex.ru>
2020-06-15 17:59:08 +0300

    Entire application test fixed

--- tests/test_entire_application.py
+++ tests/test_entire_application.py
@@ -3,6 +3,15 @@ Author: Igor
 Date: 2020.05.28
 """
 
+from children import create_app
+import pytest
+
+
+@pytest.fixture(scope='module')
+def app():
+    app = create_app(test_mode=True)
+    yield app
+
 
 def test_environment_set(app):
     assert app.testing
